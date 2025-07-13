649aedd0f2d995cdf9da48b0fe8b98044228de4e
leichester <jjleilei@gmail.com>
2025-07-12 22:09:50 -0500

    add versal

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Fun-Circle/',
   server: {
     port: 3000
   }
